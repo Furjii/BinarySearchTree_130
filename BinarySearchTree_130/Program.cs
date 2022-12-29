7ad54cffab37f3d6dd0da3fff2a9675441598f42
Furji UMY <furji.ft21@mail.umy.ac.id>
2022-12-29 17:38:11 +0700

    add main for display

--- BinarySearchTree_130/Program.cs
+++ BinarySearchTree_130/Program.cs
@@ -110,14 +110,55 @@ namespace BinarySearchTree_130
             }
             if (ptr != null)
             {
+                postorder(ptr.leftchild);
+                postorder(ptr.rightchild);
+                Console.WriteLine(ptr.info + "  ");
 
             }
         }
-    }
-    internal class program
-    {
         static void Main(string[] args)
         {
+            binarytree x = new binarytree();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("\nEnter your choice(1 - 5) :");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.WriteLine("enter a word : ");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break ;
+                    case '3':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        
+
+
+                }
+
+            }
         }
     }
 }
